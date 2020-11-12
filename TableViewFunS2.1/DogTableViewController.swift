ece5401646039c3b50da76b1268461808f00dfa3
Karsenh <KarsenHansen@gmail.com>
2020-11-12 02:38:55 -0800

    DogTableViewController conforms to UITableViewDataSource and UITableViewDelegate to update cells (Lecture 10/23)

--- TableViewFunS2.1/DogTableViewController.swift
+++ TableViewFunS2.1/DogTableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DogTableViewController: UIViewController {
+class DogTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var dogs = [Dog]()
     
@@ -23,6 +23,38 @@ class DogTableViewController: UIViewController {
         dogs.append(Dog(name: "Siggy", breed: "Malamute"))
         dogs.append(Dog(name: "Annie", breed: "Sheltie"))
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // The TableView is asking its data source:
+        /// "How many row should I put in this section?"
+        // We will only have one section
+        if section == 0 {
+            return dogs.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TableView is asking its data source
+        /// "What cell should I put at this index path?"
+        // IndexPath has 2 properties
+        /// 1. Section number
+        /// 2. Row number (corresponds to array index)
+        let row = indexPath.row
+        let dog = dogs[row]
+        
+        // Force down-cast to DogTableView cell to call update method in DogTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DogCell", for: indexPath) as! DogTableViewCell
+        
+        // Why a reusable cell?
+        // If dogs had 10k dogs listed, we wouldn't need 10k cells.
+        // iOS creates pool of 'reusable' cells and only creates enough to fill the display.
+        // As rows exit the display they enter a reusable 'pool' which they are then taken from to use for new cells entering the display during scrolling.
+        
+        cell.update(with: dog)
+        return cell
+    }
+    
 
 }
 
